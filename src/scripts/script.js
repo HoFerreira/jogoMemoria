a6896941f89bf36d8fdb53c72ac4f038340dfcb5
HoFerreira <heigthx@gmail.com>
2024-11-24 11:40:49 -0300

    Adicionando css e js

---
+++ src/scripts/script.js
@@ -0,0 +1,44 @@
+const personagens =[
+    "🦨",
+    "🦨",
+    "🦥",
+    "🦥",
+    "🦘",
+    "🦘",
+    "🐐",
+    "🐐",
+    "🦏",
+    "🦏",
+    "🦌",
+    "🦌",
+    "🐫",
+    "🐫",
+    "🦔",
+    "🦔"
+];
+let openCards = [];
+let shuffleEmojis = personagens.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < personagens.length; i++) {
+    let box = document.createElement('div');
+    box.className = 'item';
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector('.game').appendChild(box);
+    
+}
+
+function handleClick() {
+    if (openCards.length < 2) {
+        this.classList.add('boxOpen');
+        openCards.push(this);
+    }
+
+    if (openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch(){
+    
+}
